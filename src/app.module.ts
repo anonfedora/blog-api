5fc09019a8613cbd51d44c31247f5957b5a1ba36
anonfedora <musaeleazar090@gmail.com>
2024-06-20 03:40:56 +0100

    Initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { PostModule } from "./post/post.module";
+import { CommentModule } from "./comment/comment.module";
+import { CategoryModule } from "./category/category.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: [".env.localhost", ".env"]
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>("MONGO_URI"),
+            
+            })
+        }),
+        AuthModule,
+        UserModule,
+        PostModule,
+        CommentModule,
+        CategoryModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
+})
+export class AppModule {}
