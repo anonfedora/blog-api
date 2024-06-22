07349e6898751c4b08296c499a04e69760ebd072
anonfedora <musaeleazar090@gmail.com>
2024-06-22 09:39:19 +0100

    Mail

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { MailerService } from "../mailer/mailer.service";
 import { ConfigService } from "@nestjs/config";
 import { MailData } from "./interfaces/mail-data.interface";
-import path from "path";
+import * as path from "path";
 
 @Injectable()
 export class MailService {
@@ -27,6 +27,15 @@ export class MailService {
                 "mail-templates",
                 "activation.hbs"
             ),
+            /*templatePath: path.join(
+                this.configService.getOrThrow("WORK_DIR", {
+                    infer: true
+                }),
+                "src",
+                "mail",
+                "mail-templates",
+                "activation.hbs"
+            ),*/
             context: {
                 title: "Confirmation Mail",
                 url: `${this.configService.get("BASE_URL", {
@@ -36,6 +45,74 @@ export class MailService {
                 app_name: this.configService.get("APP_NAME", { infer: true }),
                 text1: "Welcome!",
                 text2: "You’re almost ready to onboard!"
+            }
+        });
+    }
+
+    async forgotPassword(
+        mailData: MailData<{ resetToken: string }>
+    ): Promise<void> {
+        const resetUrl = `${this.configService.getOrThrow(
+            "BASE_URL"
+        )}/auth/reset-password?resetToken=${mailData.data.resetToken}`;
+
+        const message = `Your password reset token is:
+${resetUrl} If you did not request this email, please ignore it.`;
+
+        this.mailerService.sendMail({
+            to: mailData.to,
+            subject: "Password Reset",
+            text: message,
+            templatePath: path.join(
+                "src",
+                "mail",
+                "mail-templates",
+                "activation.hbs"
+            ),
+            context: {
+                title: "Password Reset Token (valid for 15 minutes)",
+                url: `${this.configService.get("BASE_URL", {
+                    infer: true
+                })}/auth/reset-password?resetToken=${mailData.data.resetToken}`,
+                actionTitle: "Reset Password",
+                app_name: this.configService.get("APP_NAME", { infer: true }),
+                text1: "Reset!",
+                text2: "Password Reset!"
+            }
+        });
+    }
+
+    async resetPassword(
+        mailData: MailData<{ resetToken: string }>
+    ): Promise<void> {
+        const message = `
+        Your password reset was successful. If you did not initiate this activity, please contact ${this.configService.getOrThrow(
+            "EMAIL_USER"
+        )}.
+      `;
+
+        this.mailerService.sendMail({
+            to: mailData.to,
+            subject: "Password Reset Successful",
+            text: message,
+            templatePath: path.join(
+                this.configService.getOrThrow("WORK_DIR", {
+                    infer: true
+                }),
+                "src",
+                "mail",
+                "mail-templates",
+                "activation.hbs"
+            ),
+            context: {
+                title: "Password Reset Successful",
+                url: `${this.configService.get("BASE_URL", {
+                    infer: true
+                })}`,
+                actionTitle: "Reset Password Successful",
+                app_name: this.configService.get("APP_NAME", { infer: true }),
+                text1: "Reset!",
+                text2: "Password Reset Successful!"
             }
         });
     }
