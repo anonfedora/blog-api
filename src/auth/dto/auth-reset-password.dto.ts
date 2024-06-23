ca3927809eb3e3118494faf85dba8d39431b6c01
anonfedora <musaeleazar090@gmail.com>
2024-06-23 17:39:42 +0100

    Dto

---
+++ src/auth/dto/auth-reset-password.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, MinLength, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class AuthResetPasswordDto {
+    @ApiProperty({ type: String, example: "#1John3y24New" })
+    @IsNotEmpty()
+    @MinLength(8)
+    @IsString()
+    password: string;
+
+    @ApiProperty({ type: String, example: "eyJhc702ejjsjhhhJGYg3t70oggk" })
+    @IsNotEmpty()
+    @IsString()
+    resetToken: string;
+}
