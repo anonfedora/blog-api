c0094613939f51c85d81a1a8ca674c67585a868c
anonfedora <musaeleazar090@gmail.com>
2024-06-23 17:39:46 +0100

    Jwt

---
+++ src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ConfigService } from "@nestjs/config";
+import { JwtPayloadType } from "./types/jwt-payload.type";
+import { OrNeverType } from "src/utils/types/or-never.type";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+    constructor(private configService: ConfigService) {
+        super({
+            ignoreExpiration: false,
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.getOrThrow("JWT_SECRET", {
+                infer: true
+            })
+        });
+    }
+
+    public validate(payload: JwtPayloadType): OrNeverType<JwtPayloadType> {
+        if (!payload.id) {
+            throw new UnauthorizedException();
+        }
+        return payload;
+    }
+}
