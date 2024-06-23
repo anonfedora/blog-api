e5b67b34bc450fb21e68c88d23fa9fadaf6790fa
anonfedora <musaeleazar090@gmail.com>
2024-06-23 17:34:04 +0100

    User Auth

--- src/utils/decorators/public.ts
+++ src/utils/decorators/public.ts
@@ -1,4 +1,5 @@
-import { SetMetadata } from "@nestjs/common"
-export const PUBLIC_KEY = "isPublic"
+import { SetMetadata } from "@nestjs/common";
+export const PUBLIC_KEY = "isPublic";
+export const ROLES_KEY = "roles";
 
-export const Public = () => SetMetadata(PUBLIC_KEY, true)
+export const Public = () => SetMetadata(PUBLIC_KEY, true);
