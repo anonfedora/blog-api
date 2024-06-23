2fef5caab7d73a3f9a26cf11646261a3d70c955c
anonfedora <musaeleazar090@gmail.com>
2024-06-23 17:33:55 +0100

    Auth

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
+import validationOptions from "./utils/validation-options";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    
+
+    app.useGlobalPipes(new ValidationPipe(validationOptions));
+    app.use(cookieParser());
+
     const config = new DocumentBuilder()
-        .setTitle("Anonfedora Blog")
+        .setTitle("Anonfedora's Blog")
         .setDescription("Mini Nest Blog API description Documentation")
         .setVersion("0.1")
-        .build(); 
+        .build();
 
     const document = SwaggerModule.createDocument(app, config);
 
     SwaggerModule.setup("api", app, document);
-    
+
     await app.listen(3000);
 }
 bootstrap();
