461a69de17165243b73a5686463a88511f6f5799
anonfedora <musaeleazar090@gmail.com>
2024-06-23 17:39:34 +0100

    Dto

---
+++ src/auth/dto/auth-forgot-password.dto.ts
@@ -0,0 +1,11 @@
+ import{ IsEmail } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { lowerCaseTransformer } from 'src/utils/transformers/lower-case.transformer';
+import { ApiProperty } from "@nestjs/swagger"
+
+export class AuthForgotPasswordDto {
+  @ApiProperty({ type: String, example: "dev.mes.anonfedora@gmail.com" })
+  @Transform(lowerCaseTransformer)
+  @IsEmail()
+  email: string;
+}
