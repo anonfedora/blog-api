c2d90f740d18d157bce06235f20b1d70fac6715b
anonfedora <musaeleazar090@gmail.com>
2024-06-24 19:06:09 +0100

    Post

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -2,14 +2,15 @@ import {
     IsString,
     IsEmail,
     MinLength,
-    IsNotEmpty,
+    IsNotEmpty,IsMongoId,
     Length
 } from "class-validator";
 import { Transform } from "class-transformer";
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Category } from "../../category/schemas/category.schema";
 import { Comment } from "../../comment/schemas/comment.schema";
 import { User } from "../../user/schemas/user.schema";
+import { ObjectId } from 'mongoose';
 
 export class CreatePostDto {
     @ApiProperty({ type: String, example: "New Post" })
@@ -27,15 +28,16 @@ export class CreatePostDto {
     @Length(25, 255)
     content: string;
 
-    @ApiProperty()
-    authorId: User;
+    @ApiProperty({ example: "6678e1fc17d85f46f68ecd83" })
+    @IsMongoId()
+    authorId: ObjectId;
 
     @ApiProperty()
     categoryId: Category;
 
-    @ApiProperty()
-    comments: Comment[];
+    @ApiPropertyOptional({ description: "Collection of comments" })
+    comments: Comment;
 
-    @ApiProperty({type: Boolean, example: true})
+    @ApiProperty({ type: Boolean, example: true })
     isPublished: boolean;
 }
