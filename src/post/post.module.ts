2c075034766e9f4f51c47464824fc9c8f7b67f3f
anonfedora <musaeleazar090@gmail.com>
2024-06-24 19:06:16 +0100

    Post

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
-import { PostController } from './post.controller';
+import { Module } from "@nestjs/common";
+import { PostService } from "./post.service";
+import { PostController } from "./post.controller";
+import { PostSchema } from "./schemas/post.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  controllers: [PostController],
-  providers: [PostService],
+    imports: [
+        MongooseModule.forFeature([{ name: "Post", schema: PostSchema }])
+    ],
+    controllers: [PostController],
+    providers: [PostService]
 })
 export class PostModule {}
