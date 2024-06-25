ddb9f35ca55757c30a93c0af58ccb0052e49862e
anonfedora <musaeleazar090@gmail.com>
2024-06-25 17:21:01 +0100

    Comment

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { CommentService } from './comment.service';
-import { CommentController } from './comment.controller';
+import { Module } from "@nestjs/common";
+import { CommentService } from "./comment.service";
+import { CommentController } from "./comment.controller";
+import { CommentSchema } from "./schemas/comment.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  controllers: [CommentController],
-  providers: [CommentService],
+    imports: [
+        MongooseModule.forFeature([{ name: "Comment", schema: CommentSchema }])
+    ],
+    controllers: [CommentController],
+    providers: [CommentService]
 })
 export class CommentModule {}
