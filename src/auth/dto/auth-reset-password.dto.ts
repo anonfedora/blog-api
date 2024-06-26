08b2e316e767548191fcab3b1684a0243ecb1697
anonfedora <musaeleazar090@gmail.com>
2024-06-26 04:11:09 +0100

    Auth

--- src/auth/dto/auth-reset-password.dto.ts
+++ src/auth/dto/auth-reset-password.dto.ts
@@ -7,6 +7,12 @@ export class AuthResetPasswordDto {
     @MinLength(8)
     @IsString()
     password: string;
+    
+    @ApiProperty({ type: String, example: "#1John3y24New" })
+    @IsNotEmpty()
+    @MinLength(8)
+    @IsString()
+    confirmPassword: string;
 
     @ApiProperty({ type: String, example: "eyJhc702ejjsjhhhJGYg3t70oggk" })
     @IsNotEmpty()
