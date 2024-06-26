409359e777be794cae006813fbcc5997a04a4b85
anonfedora <musaeleazar090@gmail.com>
2024-06-26 20:04:40 +0100

    Category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { CategorySchema } from "./schemas/category.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: "Category", schema: CategorySchema }])
+    ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
