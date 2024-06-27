db1a00663adb9fa7412d7aafcf8b3ea15e320588
anonfedora <musaeleazar090@gmail.com>
2024-06-27 11:30:37 +0100

    Rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from "./user/user.module";
 import { PostModule } from "./post/post.module";
 import { CommentModule } from "./comment/comment.module";
 import { CategoryModule } from "./category/category.module";
+import { ThrottlerModule } from "@nestjs/throttler";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
@@ -15,11 +16,12 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
             isGlobal: true,
             envFilePath: [".env.localhost", ".env"]
         }),
+        ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),//Default, 1min, 10 requests
         MongooseModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: async (configService: ConfigService) => ({
-                uri: configService.get<string>("MONGO_URI"),
+                uri: configService.get<string>("MONGO_URI")
             })
         }),
         AuthModule,
