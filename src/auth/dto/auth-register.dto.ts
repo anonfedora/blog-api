162042088b2808aa12f6277323aac3191b3dbaeb
anonfedora <musaeleazar090@gmail.com>
2024-07-01 11:58:31 +0100

    Auth

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -4,7 +4,8 @@ import {
     MinLength,
     Validate,
     Length,
-    IsString
+    IsString,
+    Matches
 } from "class-validator";
 import { Transform } from "class-transformer";
 import { lowerCaseTransformer } from "src/utils/transformers/lower-case.transformer";
@@ -31,5 +32,9 @@ export class AuthRegisterDto {
     @ApiProperty({ type: String, example: "#1John3y24" })
     @IsNotEmpty()
     @MinLength(8)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'password must contain uppercase, lowercase, number and special character',
+  })
     password: string;
 }
