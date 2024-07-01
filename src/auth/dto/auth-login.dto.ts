542a2689cf0c2ca67322e5dd78edecef43d66f83
anonfedora <musaeleazar090@gmail.com>
2024-07-01 11:56:12 +0100

    Auth

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, Validate, IsEmail, IsString } from "class-validator";
+import { IsNotEmpty, Validate, IsEmail, IsString, Matches } from "class-validator";
 import { Transform } from "class-transformer";
 import { lowerCaseTransformer } from "src/utils/transformers/lower-case.transformer";
 import { ApiProperty } from "@nestjs/swagger";
@@ -14,5 +14,9 @@ export class AuthEmailLoginDto {
     @ApiProperty({ type: String, example: "#1John3y24" })
     @IsString()
     @IsNotEmpty()
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message:
+            "password must contain uppercase, lowercase, number and special character"
+    })
     password: string;
 }
