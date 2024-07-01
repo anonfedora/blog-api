1c08f6311f8e71f7f32a303ea85b1129b9813977
anonfedora <musaeleazar090@gmail.com>
2024-07-01 11:58:35 +0100

    Auth test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,89 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { UserService } from "../user/user.service";
+import { MailService } from "../mail/mail.service";
+import { ConfigService } from "@nestjs/config";
+import { HttpException, NotFoundException } from "@nestjs/common";
+import { AuthRegisterDto } from "./dto/auth-register.dto";
+/*
+jest.mock("../user/user.service");
+jest.mock("../mail/mail.service");
+jest.mock("@nestjs/config");
+*/
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe("AuthService", () => {
+    let authService: AuthService;
+    let userService: UserService;
+    let mailService: MailService;
+    let configService: ConfigService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    const mockAuthService = {
+        createUser: jest.fn()
+    };
 
-    service = module.get<AuthService>(AuthService);
-  });
+    const mockConfigService = {
+        getOrThrow: jest.fn()
+    };
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UserService,
+                    useValue: mockAuthService
+                },
+                {
+                    provide: MailService,
+                    useFactory: () => ({ userSignUp: jest.fn() })
+                },
+                {
+                    provide: ConfigService,
+                    useValue: mockConfigService
+                }
+            ]
+        }).compile();
+
+        authService = module.get<AuthService>(AuthService);
+        userService = module.get<UserService>(UserService);
+        mailService = module.get<MailService>(MailService);
+        configService = module.get<ConfigService>(ConfigService);
+    });
+
+    it("should register a new user and return the token and user", async () => {
+        const user = {
+            id: 1,
+            email: "test@example.com",
+            password: "password",
+            name: "Test User",
+            username: "testuser"
+        };
+        const dto: AuthRegisterDto = {
+            email: "test@example.com",
+            password: "password",
+            name: "Test User",
+            username: "testuser"
+        };
+        //userService.createUser.mockResolvedValueOnce(user);
+        jest.spyOn(mockAuthService, "createUser").mockResolvedValueOnce(user);
+
+        /*configService.getOrThrow
+            .mockReturnValueOnce("secret")
+            .mockReturnValueOnce("1h");*/
+        jest.spyOn(mockConfigService, "getOrThrow")
+            .mockReturnValueOnce("secret")
+            .mockReturnValueOnce("1h");
+
+        const result = await authService.register(dto);
+        expect(result).toHaveProperty("token");
+        expect(result).toHaveProperty("user");
+        expect(userService.createUser).toHaveBeenCalledWith({
+            ...dto,
+            hash: expect.any(String)
+        });
+        expect(mailService.userSignUp).toHaveBeenCalledWith({
+            to: dto.email,
+            data: { hash: expect.any(String) }
+        });
+    });
 });
