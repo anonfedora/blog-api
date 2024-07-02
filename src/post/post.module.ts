f8e1cb52b3d0d7b6114d5217cd2a77830b906b25
anonfedora <musaeleazar090@gmail.com>
2024-07-02 10:41:32 +0100

    Post

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -3,14 +3,22 @@ import { PostService } from "./post.service";
 import { PostController } from "./post.controller";
 import { PostSchema } from "./schemas/post.schema";
 import { MongooseModule } from "@nestjs/mongoose";
-import { LoggerModule } from "../logger/logger.module"
+import { LoggerModule } from "../logger/logger.module";
+import { MulterModule } from "@nestjs/platform-express";
+import { CloudinaryModule } from "../cloudinary/cloudinary.module";
+import { CloudinaryService } from "../cloudinary/cloudinary.service";
+import * as multer from "multer";
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: "Post", schema: PostSchema }]),
-        LoggerModule
+        MulterModule.register({
+            storage: multer.memoryStorage()
+        }),
+        LoggerModule,
+        CloudinaryModule
     ],
     controllers: [PostController],
-    providers: [PostService]
+    providers: [PostService, CloudinaryService]
 })
 export class PostModule {}
