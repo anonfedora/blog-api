ecedd7ba8c09124780cb0edd049647d632d4cb3b
Musa Eleazar <musaeleazar090@gmail.com>
2024-08-03 14:52:39 +0100

    Refactor PostServic

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -12,145 +12,144 @@ import { NotificationService } from "../notification/notification.service";
 
 @Injectable()
 export class PostService {
-    constructor(
-        @InjectModel("Post") private readonly postModel: Model<PostDocument>,
-        private readonly cloudinaryService: CloudinaryService,
-        private readonly notificationService: NotificationService
-    ) {}
+  constructor(
+    @InjectModel("Post") private readonly postModel: Model<PostDocument>,
+    private readonly cloudinaryService: CloudinaryService,
+    private readonly notificationService: NotificationService
+  ) {}
 
-    async create(
-        userId: string,
-        createPostDto: CreatePostDto,
-        file: Express.Multer.File
-    ): Promise<PostDocument> {
-        let imagePath: string = null;
-        if (file) {
-            const uploadResult = await this.cloudinaryService.uploadImage(file);
-            imagePath = uploadResult.secure_url;
-        }
-
-        const newPost = await this.postModel.create({
-            authorId: userId,
-            ...createPostDto,
-            image: imagePath
-        });
-        //  return newPost;
-        return await newPost.save();
+  async create(
+    userId: string,
+    createPostDto: CreatePostDto,
+    file: Express.Multer.File
+  ): Promise<PostDocument> {
+    let imagePath: string = null;
+    if (file) {
+      const uploadResult = await this.cloudinaryService.uploadImage(file);
+      imagePath = uploadResult.secure_url;
     }
 
-    async findAll(page: number = 1, limit: number = 10) {
-        const skip = (page - 1) * limit;
-        const total = await this.postModel.countDocuments();
-        const posts = await this.postModel
-            .find()
-            .populate("comments categoryId", "name")
-            .skip(skip)
-            .limit(limit)
-            .sort({ createdAt: 1 })
-            .exec();
-        return {
-            data: posts,
-            currentPage: page,
-            totalPages: Math.ceil(total / limit),
-            totalPosts: total
-        };
-    }
+    const newPost = await this.postModel.create({
+      authorId: userId,
+      ...createPostDto,
+      image: imagePath,
+    });
+    //  return newPost;
+    return await newPost.save();
+  }
 
-    async findUserPost(id: string): Promise<Post | null> {
-        return await this.postModel
-            .findById({ authorId: id })
-            .populate("posts categoryId", "name")
-            .exec();
-    }
+  async findAll(page: number = 1, limit: number = 10) {
+    const skip = (page - 1) * limit;
+    const total = await this.postModel.countDocuments();
+    const posts = await this.postModel
+      .find()
+      .populate("comments categoryId", "name")
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: 1 })
+      .exec();
+    return {
+      data: posts,
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalPosts: total,
+    };
+  }
 
-    // TODO - Retrieve all comments for a specific post.
-    async getCommentsByPost(postId: string) {
-        return await this.postModel
-            .find({ _id: postId })
-            .populate("comments categoryId", "content name")
-            .exec();
-    }
+  async findUserPost(id: string): Promise<Post | null> {
+    return await this.postModel
+      .findById({ authorId: id })
+      .populate("posts categoryId", "name")
+      .exec();
+  }
 
-    async findOne(id: string): Promise<Post | null> {
-        return await this.postModel
-            .findById(id)
-            .populate("comments categoryId", "content likesCount dislikesCount name")
-            .exec();
-    }
+  // TODO - Retrieve all comments for a specific post.
+  async getCommentsByPost(postId: string) {
+    return await this.postModel
+      .find({ _id: postId })
+      .populate("comments categoryId", "content name")
+      .exec();
+  }
 
-    async update(
-        postId: string,
-        updatePostDto: Partial<UpdatePostDto>,
-        userId: string
-    ): Promise<Post | null> {
-        const post = await this.postModel.findById(postId);
+  async findOne(id: string): Promise<Post | null> {
+    return await this.postModel
+      .findById(id)
+      .populate("comments categoryId", "content likesCount dislikesCount name")
+      .exec();
+  }
 
-        /*console.log(post);
+  async update(
+    postId: string,
+    updatePostDto: Partial<UpdatePostDto>,
+    userId: string
+  ): Promise<Post | null> {
+    const post = await this.postModel.findById(postId);
+
+    /*console.log(post);
         console.log(post.authorId + "," + userId + "......");
         console.log(post.authorId == userId);*/
-        if (post.authorId == userId)
-            return await this.postModel.findByIdAndUpdate(
-                postId,
-                updatePostDto,
-                { new: true }
-            );
-    }
+    if (post.authorId == userId)
+      return await this.postModel.findByIdAndUpdate(postId, updatePostDto, {
+        new: true,
+      });
+  }
 
-    async search(query: string): Promise<Post[] | null> {
-        const results = await this.postModel
-            .find({
-                $or: [
-                    { title: { $regex: query, $options: "i" } },
-                    { content: { $regex: query, $options: "i" } }
-                ]
-            })
-            .exec();
-        return results;
-    }
+  async search(query: string): Promise<Post[] | null> {
+    const results = await this.postModel
+      .find({
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { content: { $regex: query, $options: "i" } },
+        ],
+      })
+      .exec();
+    return results;
+  }
 
-    // TODO - Retrieve user by userid and setn notificationService name to user.name [likePost, dislikePost]
-    async likePost(postId: string, userId: string): Promise<Post> {
-        const post = await this.postModel.findById(postId);
-        if (!post.likes.includes(userId) && post.dislikes.includes(userId)) {
-            post.likes.push(userId);
-            post.dislikes = post.dislikes.filter(id => id.toString() != userId);
-            post.likesCount++;
-            post.dislikesCount--;
-        } else if (!post.likes.includes(userId)) {
-            post.likes.push(userId);
-            post.dislikes = post.dislikes.filter(id => id.toString() != userId);
-            post.likesCount++;
-        }
-        await this.notificationService.createNotification(
-            post.authorId,
-            `${userId} liked your post`
-        );
-        return post.save();
+  // TODO - Retrieve user by userid and setn notificationService name to user.name [likePost, dislikePost]
+  async likePost(postId: string, userId: string): Promise<Post> {
+    const post = await this.postModel.findById(postId);
+    if (!post.likes.includes(userId) && post.dislikes.includes(userId)) {
+      post.likes.push(userId);
+      post.dislikes = post.dislikes.filter((id) => id.toString() != userId);
+      post.likesCount++;
+      post.dislikesCount--;
+    } else if (!post.likes.includes(userId)) {
+      post.likes.push(userId);
+      post.dislikes = post.dislikes.filter((id) => id.toString() != userId);
+      post.likesCount++;
     }
+    console.log("PostService - Like Post: post.authorId", post.authorId);
+    await this.notificationService.createNotification(
+      post.authorId,
+      `${userId} liked your post`
+    );
+    return post.save();
+  }
 
-    async dislikePost(postId: string, userId: string): Promise<Post> {
-        const post = await this.postModel.findById(postId);
-        if (!post.dislikes.includes(userId) && post.likes.includes(userId)) {
-            post.dislikes.push(userId);
-            post.likes = post.likes.filter(id => id.toString() !== userId);
-            post.dislikesCount++;
-            post.likesCount--;
-        }
-        if (!post.dislikes.includes(userId)) {
-            post.dislikes.push(userId);
-            post.likes = post.likes.filter(id => id.toString() !== userId);
-            post.dislikesCount++;
-        }
-        await this.notificationService.createNotification(
-            post.authorId,
-            `${userId} disliked your post`
-        );
-        return post.save();
+  async dislikePost(postId: string, userId: string): Promise<Post> {
+    const post = await this.postModel.findById(postId);
+    if (!post.dislikes.includes(userId) && post.likes.includes(userId)) {
+      post.dislikes.push(userId);
+      post.likes = post.likes.filter((id) => id.toString() !== userId);
+      post.dislikesCount++;
+      post.likesCount--;
     }
+    if (!post.dislikes.includes(userId)) {
+      post.dislikes.push(userId);
+      post.likes = post.likes.filter((id) => id.toString() !== userId);
+      post.dislikesCount++;
+    }
+    await this.notificationService.createNotification(
+      post.authorId,
+      `${userId} disliked your post`
+    );
+    return post.save();
+  }
 
-    async remove(postId: string, userId: string): Promise<Post | null> {
-        const post = await this.postModel.findById(postId);
-        if (post.authorId == userId)
-            return await this.postModel.findByIdAndDelete(postId);
-    }
+  async remove(postId: string, userId: string): Promise<Post | null> {
+    const post = await this.postModel.findById(postId);
+    if (post.authorId == userId)
+      return await this.postModel.findByIdAndDelete(postId);
+  }
 }
