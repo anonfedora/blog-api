8d312b240e73e3bfeaa55020fcb7d45809527022
Musa Eleazar <musaeleazar090@gmail.com>
2024-08-03 14:55:41 +0100

    Notification empty response fix - await keyword

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,27 +1,31 @@
-import { Controller, Get, Param, Patch, Body, Req, UseGuards} from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Body,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
 import { NotificationService } from "./notification.service";
 import { Notification } from "./schemas/notification.schema";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
 @Controller("notifications")
 export class NotificationController {
-    constructor(private readonly notificationService: NotificationService) {}
+  constructor(private readonly notificationService: NotificationService) {}
 
-    @UseGuards(JwtAuthGuard)
-    @Get()
-    /*@Get(":userId")*/
-    async getUserNotifications(
-        @Req() req
-        /*@Param("userId") userId: string*/
-    ): Promise<Notification[]> {
-        const userId = req.user.sub;
-        return this.notificationService.getNotificationsForUser(userId);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getUserNotifications(@Req() req): Promise<Notification[]> {
+    const userId = req.user.sub;
+    return this.notificationService.getNotificationsForUser(userId);
+  }
 
-    @Patch(":notificationId/read")
-    async markNotificationAsRead(
-        @Param("notificationId") notificationId: string
-    ): Promise<Notification> {
-        return this.notificationService.markAsRead(notificationId);
-    }
+  @Patch(":notificationId/read")
+  async markNotificationAsRead(
+    @Param("notificationId") notificationId: string
+  ): Promise<Notification> {
+    return this.notificationService.markAsRead(notificationId);
+  }
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,25 +1,40 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Notification, NotificationDocument } from "./schemas/notification.schema";
+import { Model, Types } from "mongoose";
+import {
+  Notification,
+  NotificationDocument,
+} from "./schemas/notification.schema";
 
 @Injectable()
 export class NotificationService {
-    constructor(
-        @InjectModel("Notification") private readonly notificationModel: Model<NotificationDocument>
-    ) {}
+  constructor(
+    @InjectModel("Notification")
+    private readonly notificationModel: Model<NotificationDocument>
+  ) {}
 
-    async createNotification(userId: string, message: string): Promise<Notification> {
-        const newNotification = await this.notificationModel.create({userId, message });
-        return await newNotification.save();
-    }
+  async createNotification(
+    userId: string,
+    message: string
+  ): Promise<Notification> {
+    const newNotification = await this.notificationModel.create({
+      userId,
+      message,
+    });
+    return await newNotification.save();
+  }
 
-    async getNotificationsForUser(userId: string): Promise<Notification[]> {
-        return this.notificationModel.find({ userId }).sort({ createdAt: -1 }).exec();
-        /*return this.notificationModel.find({ userId }).sort({ createdAt: -1 }).exec();*/
-    }
+  async getNotificationsForUser(userId: string): Promise<Notification[]> {
+    return await this.notificationModel
+      .find({ userId: new Types.ObjectId(userId) })
+      .sort({ createdAt: -1 })
+      .exec();
+    /*return this.notificationModel.find({ userId }).sort({ createdAt: -1 }).exec();*/
+  }
 
-    async markAsRead(notificationId: string): Promise<Notification> {
-        return this.notificationModel.findByIdAndUpdate(notificationId, { isRead: true }, { new: true }).exec();
-    }
+  async markAsRead(notificationId: string): Promise<Notification> {
+    return this.notificationModel
+      .findByIdAndUpdate(notificationId, { isRead: true }, { new: true })
+      .exec();
+  }
 }
